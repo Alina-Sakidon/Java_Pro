e326f69f871739221b567b504327044e6d58657b
Alina <alinasakidon@gmail.com>
2023-11-22 12:34:33 +0200

    update name

--- src/HW2/Main.java
+++ src/HW2/Main.java
@@ -4,7 +4,7 @@ import HW2.Employee.Employee;
 
 public class Main {
     public static void main(String[] args) {
-        Employee Alina = new Employee("Sakidon Alina", "automation QA", "alinasakidon@gmail.com", "+380994184102", 32);
+        Employee alina = new Employee("Sakidon Alina", "automation QA", "alinasakidon@gmail.com", "+380994184102", 32);
         Car honda = new Car();
         honda.start();
     }
