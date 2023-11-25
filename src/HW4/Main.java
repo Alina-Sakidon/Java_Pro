d7bca0916266c40e62bcd04ced38cc9d52851034
Alina <alinasakidon@gmail.com>
2023-11-25 13:12:57 +0200

    add 1-2 HW4

---
+++ src/HW4/Main.java
@@ -0,0 +1,12 @@
+package HW4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SomeStringMethods someStringMethods = new SomeStringMethods();
+
+        System.out.println(someStringMethods .findSymbolOccurance("Test was created without errors", 'w'));
+
+        System.out.println(someStringMethods.findWordPosition("My little fried seats on little bag", "little"));
+    }
+}
---
+++ src/HW4/SomeStringMethods.java
@@ -0,0 +1,24 @@
+package HW4;
+
+public class SomeStringMethods {
+
+    public int findSymbolOccurance(String someString, char symbol) {
+        int count = 0;
+        for (int i = 0; i < someString.length(); i++) {
+            if (someString.charAt(i) == symbol) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int findWordPosition(String source, String target) {
+        String[] listWord = source.split(" ");
+        for (int i = 0; i < listWord.length; i++) {
+            if (listWord[i].equals(target)) {
+              return   i + 1;
+            }
+        }
+        return -1;
+    }
+}
