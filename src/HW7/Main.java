e127f97d9d8919f1f01f1c6fe9a304ec0aa41e46
Alina <alinasakidon@gmail.com>
2023-12-09 12:46:35 +0200

    add task2 HW7

--- src/HW7/Main.java
+++ src/HW7/Main.java
@@ -1,5 +1,6 @@
 package HW7;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -12,9 +13,18 @@ public class Main {
                 "item9", "item9", "item10", "item10", "item10", "item10"));
 
         System.out.println(countOccurrence(rows, "item10"));
+
+        //Task2
+        String[] lines = {"test1 string line", "test2", "test3", "test4", "test4"};
+
+        System.out.println(toList(lines));
     }
 
     static int countOccurrence(List<String> rows, String value) {
         return Collections.frequency(rows, value);
+    }
+
+    static ArrayList<String> toList(String... lines) {
+        return new ArrayList<>(Arrays.asList(lines));
     }
 }
