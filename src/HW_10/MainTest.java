5810bb57ad35bf40a1d5b8508b162e837effaddf
Alina <alinasakidon@gmail.com>
2024-01-02 18:42:54 +0200

    add changes

--- src/HW_10/MainTest.java
+++ src/HW_10/MainTest.java
@@ -1,13 +1,9 @@
 package HW_10;
 
-import javax.xml.crypto.Data;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -36,17 +32,21 @@ public class MainTest {
         List<Product> listOfProducts = Arrays.asList(prod1, prod2, prod3, prod4);
 
         //Task1.2
+        System.out.println("Task1.2 All books with price > 250");
         System.out.println(findProductsByTypeAndPrice(listOfProducts, BOOK, 250f));
 
         //Task2.2
+        System.out.println("Task2.2 All books with discount");
         System.out.println(applyDiscount(listOfProducts));
 
         //Task3.3
+        System.out.println("Task3.2 Find book with min price");
         System.out.println(findProductWithMinPrice(listOfProducts));
 
-      /*  // Task3.3
+        // Task3.3
+        System.out.println("Task3.3 Put exception if category not found");
         List<Product> listOfProducts2 = Arrays.asList(prod3, prod4);
-        System.out.println(findProductsByType(listOfProducts2, BOOK));*/
+        //System.out.println(findProductsByType(listOfProducts2, BOOK));
 
         //Task4.2
         Product prodA = Product.builder()
@@ -56,31 +56,50 @@ public class MainTest {
                 .build();
         Product prodB = Product.builder()
                 .type(BOOK)
-                .addingDate(LocalDate.of(2024, Month.MARCH, 18))
-                .price(305.10f)
+                .addingDate(LocalDate.of(2024, Month.MARCH, 10))
+                .price(301.10f)
                 .build();
         Product prodC = Product.builder()
                 .type(TOY)
                 .price(13.15f)
-                .addingDate(LocalDate.of(2024, Month.DECEMBER, 21))
+                .addingDate(LocalDate.of(2024, Month.DECEMBER, 07))
                 .build();
         Product prodD = Product.builder()
                 .type(TOY)
                 .price(85.15f)
-                .addingDate(LocalDate.of(2024, Month.FEBRUARY, 16))
+                .addingDate(LocalDate.of(2024, Month.FEBRUARY, 02))
                 .build();
-        List<Product> listOfProducts2 = Arrays.asList(prodA, prodB, prodC, prodD);
+        Product prodG = Product.builder()
+                .type(BOOK)
+                .price(5f)
+                .addingDate(LocalDate.of(2023, Month.FEBRUARY, 04))
+                .build();
+        Product prodQ = Product.builder()
+                .type(BOOK)
+                .price(10f)
+                .addingDate(LocalDate.of(2024, Month.FEBRUARY, 05))
+                .build();
+        Product prodZ = Product.builder()
+                .type(BOOK)
+                .price(20f)
+                .addingDate(LocalDate.of(2024, Month.FEBRUARY, 06))
+                .build();
+        List<Product> listOfProducts3 = Arrays.asList(prodA, prodB, prodC, prodD, prodG, prodQ, prodZ);
 
-        sortProductsByDate(listOfProducts2).stream().limit(1).forEach(System.out::println);
+        System.out.println("Task4.2 Last 3  added products");
+        sortProductsByDate(listOfProducts3).stream().limit(3).forEach(System.out::println);
+
+        // Task 5.2
+        System.out.println("Task5.2 Sum of Book products of the last year with price< 75");
+        System.out.println(getSumProductsLastYear(listOfProducts3));
     }
 
     public static List<Product> findProductsByTypeAndPrice(List<Product> products, String neededType, Float neededPrice) {
-        return products.stream().filter(p -> p.getType().equals(neededType)).filter(p -> p.getPrice() > neededPrice).collect(Collectors.toList());
+        return products.stream().filter(p -> p.getType().equals(neededType) && p.getPrice() > neededPrice).collect(Collectors.toList());
     }
 
     public static List<Product> applyDiscount(List<Product> listOfProducts) {
         List<Product> productsWithDiscount = findProductWithDiscount(listOfProducts);
-        productsWithDiscount.forEach(p -> p.setDiscount(true));
         productsWithDiscount.forEach(p -> p.setPrice(p.getPrice() - p.getPrice() * 0.1f));
         return productsWithDiscount;
     }
@@ -91,8 +110,8 @@ public class MainTest {
                 .collect(Collectors.toList());
     }
 
-    private  List<Product> findProductsByType(List<Product> products, String type) {
-        return products.stream().filter(p -> p.getType().contains(type))
+    private static List<Product> findProductsByType(List<Product> products, String type) {
+        return products.stream().filter(p -> p.getType().equals(type))
                 .collect(Collectors.collectingAndThen(Collectors.toList(), Optional::of))
                 .filter(l -> !l.isEmpty())
                 .orElseThrow(() -> new NoSuchElementException(
@@ -104,7 +123,15 @@ public class MainTest {
     }
 
     public static List<Product> sortProductsByDate(List<Product> listOfProducts) {
-        return applyDiscount(listOfProducts).stream().sorted(Comparator.comparing(Product::getAddingDate).reversed()).collect(Collectors.toList());
+        return listOfProducts.stream().sorted(Comparator.comparing(Product::getAddingDate).reversed()).collect(Collectors.toList());
     }
 
+    public static double getSumProductsLastYear(List<Product> listOfProducts) {
+        return listOfProducts.stream()
+                .filter(p -> p.getType().equals(BOOK))
+                .filter(p -> p.getAddingDate().isAfter(LocalDate.of(2023, Month.DECEMBER, 31)))
+                .filter(p -> p.getPrice() < 75)
+                .mapToDouble(p -> p.getPrice())
+                .sum();
+    }
 }
--- src/HW_10/Product.java
+++ src/HW_10/Product.java
@@ -1,6 +1,5 @@
 package HW_10;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,9 +7,6 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
@@ -21,14 +17,9 @@ public class Product {
 
     private String type;
     private float price;
-    private boolean discount;
     private LocalDate addingDate;
 
     public static class Type {
         public static final String BOOK = "Book", TOY = "Toy";
-    }
-
-    public  boolean isContainsDiscount(String type){
-        return getType().equals(type);
     }
 }
